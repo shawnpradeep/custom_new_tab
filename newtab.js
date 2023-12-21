81da9bc54fd6507a0df87e149a97e35a33c886a1
Shawn Pradeep <shawnprad@hotmail.com>
2023-12-21 10:21:29 -0500

    fixed color bug

--- newtab.js
+++ newtab.js
@@ -3,10 +3,10 @@ let hours = new Date().getHours();
 if (hours > 17 || hours < 7) {
   document.documentElement.style.backgroundColor = "#101011";
 } else {
-  document.body.style.backgroundColor = "#f8fcf8";
+  document.documentElement.style.backgroundColor = "#f8fcf8";
 }
 
 chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-  var customDomain = "https://shawnpradeep.com";
+  let customDomain = "https://shawnpradeep.com";
   chrome.tabs.update(tabs[0].id, { url: customDomain });
 });
